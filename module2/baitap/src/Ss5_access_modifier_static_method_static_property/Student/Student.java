f272873be7de7afc93590ffdea3dff1cb7b0c396
professinal12 <50769481+professinal12@users.noreply.github.com>
2022-09-14 10:18:12 +0700

    baitap

---
+++ module2/baitap/src/Ss5_access_modifier_static_method_static_property/Student/Student.java
@@ -0,0 +1,20 @@
+package Ss5_access_modifier_static_method_static_property.Student;
+
+public class Student {
+    public String name = "john";
+    public String name1="huy";
+    private String classes = "C02";
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+
+}
+
+
+
---
+++ module2/baitap/src/Ss5_access_modifier_static_method_static_property/access_modifier/TestCircle.java
@@ -0,0 +1,9 @@
+package Ss5_access_modifier_static_method_static_property.access_modifier;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle =new Circle();
+        System.out.println(circle.getRadius());
+        System.out.println(circle.getColor());
+    }
+}
