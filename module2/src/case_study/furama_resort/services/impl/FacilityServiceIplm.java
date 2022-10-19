0f04a76e51b7f87285668b6e2622a8d0cdb87b76
professinal12 <50769481+professinal12@users.noreply.github.com>
2022-10-19 21:08:12 +0700

    19/10

--- module2/src/case_study/furama_resort/services/impl/FacilityServiceIplm.java
+++ module2/src/case_study/furama_resort/services/impl/FacilityServiceIplm.java
@@ -1,11 +1,24 @@
 package case_study.furama_resort.services.impl;
 
+import case_study.furama_resort.facilities.Facility;
 import case_study.furama_resort.services.FacilityService;
 
+import java.util.*;
+
 public class FacilityServiceIplm implements FacilityService {
+    private static final LinkedHashMap<Integer, Facility> facilitiesList = new LinkedHashMap<Integer, Facility>();
+    private static final Scanner scanner = new Scanner(System.in);
+
+    static {
+
+    }
+
     @Override
     public void display() {
-
+        Set<Integer> keys = facilitiesList.keySet();
+        for (Integer key : keys) {
+            System.out.println("Key: " + key + " | Value: " + facilitiesList.get(key));
+        }
     }
 
     @Override
