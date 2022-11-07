da4a2be4314c2ebe1959fef3dd1e2c3d4f71a16b
professinal12 <50769481+professinal12@users.noreply.github.com>
2022-11-07 09:47:47 +0700

    7/11

---
+++ module2/src/ss16_io_binary_file_and_serialization/bai_tap/manager_product/TestMain.java
@@ -0,0 +1,18 @@
+package ss16_io_binary_file_and_serialization.bai_tap.manager_product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestMain {
+    public static void main(String[] args) {
+        List<Product> productList = new ArrayList<>();
+        productList.add(new Product("TV22", "Tivi", "Sony", 200, "Black Color"));
+        productList.add(new Product("DL16", "Display", "ASUS", 50, "FHD/16 Inch"));
+        productList.add(new Product("WM22", "Wash Machine", "Panasonic", 100, "Vertical"));
+        productList.add(new Product("LS22", "Loudspeaker", "JBL", 50, "Water Resistance"));
+
+        IoBinary.writeFileOptimize(productList, "ProductList.txt");
+        IoBinary.readFile("ProductList.txt");
+        productList.forEach(System.out::println);
+    }
+}
